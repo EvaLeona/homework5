5d0a616b1602968eb3e32b49a8569083b8805162
Eva <eva.leona@mail.ru>
2022-11-29 10:45:48 +0300

    diffficult

---
+++ lesson5_difficult_task.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int[] array = new int[5];
+        Random rnd = new Random();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(0, 999); 
+        }
+        
+        int more = 0;
+        int less = 0;
+        int a = 0;
+        int e = a + 1;
+        for (int n = array.Length; n > 0; n--)
+        {
+            while (e < n)
+            {
+                if (array[a] < array[e])
+                {
+                    more = array[e];
+                    less = array[a];
+                    array[e] = less;
+                    array[a] = more;
+                }
+                a++;
+                e++;
+            }
+            a = 0;
+            e = a + 1;
+        }
+        foreach (var num in array)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
