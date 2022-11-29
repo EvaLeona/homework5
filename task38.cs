ce7b28de5c9c473b8cf812eb9f0754fe4d513d66
Eva <eva.leona@mail.ru>
2022-11-29 10:44:59 +0300

    task38

---
+++ task38.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int[] array = new int[5];
+        Random rnd = new Random();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(0, 999); 
+        }
+        
+        int min = array[0];
+        int max = array[0];
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] < min)
+                min = array[i];
+            if (array[i] > max)
+                max = array[i];
+        }
+        max -= min;
+
+        Console.WriteLine(max);
+    }
+}
