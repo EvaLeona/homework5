f6a4e3bc422633602e54c8cda229bd1f4b4fa95f
Eva <eva.leona@mail.ru>
2022-11-29 10:42:10 +0300

    task34

---
+++ task34.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int[] array = new int[4];
+        Random rnd = new Random();
+    
+        int even = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(100, 999); 
+        
+            if (array[i] % 2 == 0)
+            even++;
+        }
+        
+        Console.WriteLine(even);
+    }
+}
