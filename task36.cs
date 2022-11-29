297b4bbf85e544da898dcd3bf6ee6cf7416e302b
Eva <eva.leona@mail.ru>
2022-11-29 10:43:26 +0300

    task36

---
+++ task36.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int[] array = new int[4];
+        Random rnd = new Random();
+    
+        int sum = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(-99, 99); 
+        }
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i % 2 != 0)
+            sum += array[i];
+        }
+
+        Console.WriteLine(sum);
+    }
+}
